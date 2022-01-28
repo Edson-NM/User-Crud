5e595a121ac1bef93ea6d3ec569e38878558156f
Edson-NM <edson_nogueda@outlook.com>
2022-01-28 01:16:46 -0600

    Avance: Funcionalidad terminda. Pendiente: Estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import "./App.css";
+import UsersForm from "./components/UsersForm";
+import UsersList from "./components/UsersList";
+import { useForm } from "react-hook-form";
 
 function App() {
+  const { reset } = useForm();
+
   //useState
   const [users, setUsers] = useState([]);
+  const [userSelected, setUserSelected] = useState(null);
 
   //useEffect
   useEffect(() => {
@@ -13,7 +19,60 @@ function App() {
       .then((res) => setUsers(res.data));
   }, []);
 
-  return <div className="App"></div>;
+  //functions
+  const getUsers = () => {
+    axios
+      .get("https://users-crud1.herokuapp.com/users/")
+      .then((res) => setUsers(res.data));
+  };
+
+  const addUser = (data) => {
+    // axios
+    //   .post("https://users-crud1.herokuapp.com/users/", data)
+    //   .then(() => getUsers());
+    console.log(data);
+  };
+
+  const deleteUser = (userId) => {
+    console.log(userId);
+    axios
+      .delete(`https://users-crud1.herokuapp.com/users/${userId}/`)
+      .then(() => getUsers());
+  };
+
+  const selectUser = (user) => {
+    setUserSelected(user);
+  };
+
+  const desSelectuser = () => {
+    setUserSelected(null);
+  };
+
+  const updateUser = (updatedData) => {
+    axios
+      .put(
+        `https://users-crud1.herokuapp.com/users/${userSelected.id}/`,
+        updatedData
+      )
+      .then(() => getUsers());
+  };
+
+  return (
+    <div className="App" style={{ display: "flex", height: "100vh" }}>
+      <UsersList
+        users={users}
+        deleteUser={deleteUser}
+        selectUser={selectUser}
+      />
+      <UsersForm
+        addUser={addUser}
+        updateUser={updateUser}
+        userSelected={userSelected}
+        desSelectuser={desSelectuser}
+        setUserSelected={setUserSelected}
+      />
+    </div>
+  );
 }
 
 export default App;
--- src/components/UsersForm.jsx
+++ src/components/UsersForm.jsx
@@ -1,7 +1,78 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
 
-const UsersForm = () => {
-  return <div></div>;
+const UsersForm = ({
+  addUser,
+  updateUser,
+  userSelected,
+  desSelectuser,
+  setUserSelected,
+}) => {
+  const defaultUser = {
+    email: "",
+    password: "",
+    first_name: "",
+    last_name: "",
+    birthday: "",
+  };
+
+  const { register, handleSubmit, reset } = useForm();
+
+  useEffect(() => {
+    if (userSelected?.first_name) {
+      const autofill = () => {
+        reset({
+          email: userSelected.email,
+          password: userSelected.password,
+          first_name: userSelected.first_name,
+          last_name: userSelected.last_name,
+          birthday: userSelected.birthday,
+        });
+      };
+      autofill();
+    } else {
+      reset(defaultUser);
+    }
+  }, [userSelected]);
+
+  const submit = (data) => {
+    if (userSelected?.first_name) {
+      updateUser(data);
+      reset(defaultUser);
+      setUserSelected(null);
+    } else {
+      addUser(data);
+      reset(defaultUser);
+    }
+  };
+
+  return (
+    <div style={{ width: "50%" }}>
+      <form onSubmit={handleSubmit(submit)}>
+        <input
+          type="text"
+          placeholder="First Name"
+          {...register("first_name")}
+        />
+        <input type="text" placeholder="Last Name" {...register("last_name")} />
+        <input type="email" placeholder="Email" {...register("email")} />
+        <input
+          type="password"
+          placeholder="Password"
+          {...register("password")}
+        />
+        <input type="date" placeholder="Birthday" {...register("birthday")} />
+        {userSelected !== null ? (
+          <>
+            <button>Update</button>
+            <button onClick={desSelectuser}>Cancel</button>
+          </>
+        ) : (
+          <button>Create</button>
+        )}
+      </form>
+    </div>
+  );
 };
 
 export default UsersForm;
